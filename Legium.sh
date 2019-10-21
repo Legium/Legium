5a03e6250d244a3e6c7418f8caea7cc9de888367
Legium <56835584+Legium@users.noreply.github.com>
2019-10-21 22:03:55 +0700

    Add files via upload

---
+++ Legium.sh
@@ -0,0 +1,437 @@
+#!/bin/sh
+
+clear
+
+echo '╔═══════════╗
+╔═╝███████████╚═╗
+╔╝███████████████╚╗
+║█████████████████║
+║█████████████████║
+║█████████████████║
+║█╔█████████████╗█║
+╚╦╝███▒▒███▒▒███╚╦╝
+╔╝██▒▒▒▒███▒▒▒▒██╚╗
+║██▒▒▒▒▒███▒▒▒▒▒██║
+║██▒▒▒▒█████▒▒▒▒██║
+╚╗███████████████╔╝
+ ═╬══╦╝██▒█▒██╚╦══╝.▒..
+║█║══║█████████║　...▒.
+║█║══║█║██║██║█║　.▒..
+║█║══╚═╩══╩╦═╩═╩═╦╗▒.
+ ╔╝█╚══╦═╦══╦╩═╦═╦═╩╝
+╔╝█████║█║██║██║█║
+║██████║█████████║'
+echo '\33[0;36m##############################################################################\033[1;0'
+echo '			\033[1;31mCYBER LEGIUM CRIME\033[1;0m'
+echo '\033[0;33mAUTHOR    :  \033[1;37mNoval Hermansyah'
+echo '\33[0;36mfacebook   :  \033[1;37mhttps://m.facebook.com/papan.tulis\033[1;0m'
+echo '			\033[1;31mCYBER LEGIUM CRIME\033[1;0m'
+echo '\33[0;36m#############################################################################\033[1;0m'
+
+sleep 1
+
+clear
+
+echo '\33[32;1m____________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║  ╔╗╔╔═╗╔╦╗╔═╗
+ ╔════════════╗ ║║║╠═╣║║║╠═╣
+ ╚════════════╝ ╝╚╝╩ ╩╩ ╩╩ ╩
+  ║██████████╚╗ ╦╔═╔═╗╔╦╗╦ ╦
+  ║██╔══╗█╔═╗█║ ╠╩╗╠═╣║║║║ ║
+  ║██║╬╔╝█╚╗║█║ ╩ ╩╩ ╩╩ ╩╚═╝
+  ║██╚═╝█║█╚╝█║ Isi Dengan Nama Kalian *_*
+  ╚╗█████████═╝
+   ╚╗║╠╩╩╩╩╩╝
+    ║║┈┈┈█▐█████▒.｡oO
+    ║██╠╦╦╦╗
+    ╚╗██████ Author:Mr Lgiumn
+     ╚════╝ TEAM CYBER LEGIUM
+<════════════════X═════════════════>\33[37;1m'
+read -p 'Masukkan Nama Anda : ' nama1;
+sleep 1
+echo 'Selamat Datang '$nama1
+echo '\33[32;1m#############################################################################\033[1;0m'
+
+
+ulang='y'
+while [ $ulang = 'y' ];
+do
+
+sleep 1
+
+echo '\33[31;1mSILAHKAN NIKMATI TOOLS DI BAWAH\33[37;1m'
+echo '\33[1;33m╔════╗╔══════════════════════╗╔══════════╗
+║{NO}║║  { KUMPULAN TOOLS }  ║║{ STATUS }║
+╚════╝╚════════════════════> ╚╝ <════════╝
+╔════╗╔══════════════════════╗
+║ 1  ║║    ( Red-Hawk )      ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 2  ║║    ( DarkFb )        ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 3  ║║   ( lacak lokasi )   ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 4  ║║      ( OSIF )        ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 5  ║║  ( Hack Instagram )  ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 6  ║║  ( spam call masal ) ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 7  ║║    ( Spam SMS )      ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 8  ║║     ( Auto MBF )     ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 9  ║║     ( Katoolin )     ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 10 ║║     ( Spam v1 )      ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 11 ║║    ( nuyul CAPING )  ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 12 ║║     ( VIRUS HP )     ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 13 ║║   ( Nuyul cashtree ) ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 14 ║║  ( auto like insta ) ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 15 ║║( H target no brute ) ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 16 ║║   ( Sadap Wa v2 )    ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 17 ║║    ( metasploit )    ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 18 ║║    ( DDOS HAMMER )   ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 19 ║║     ( Virtext )      ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 20 ║║     ( phising )      ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 19 ║║  ( tombol special )  ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 22 ║║  ( UN FOLLOW FB  )   ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 23 ║║   ( Jadwal Sholat )  ║
+╚════╝╚══════════════════════╝
+╔════╗╔══════════════════════╗
+║ 0  ║║      ( EXIT )        ║
+╚════╝╚══════════════════════╝\33[37;1m'
+
+read -p 'Masukkan Pilhan anda : ' pil;
+  #kondi jika input = 1 maka akan menginstall redhawk
+  if [ $pil = '1' ]
+  then
+      echo '\33[31;1minstalling Red Hawk\33[37;1m'
+      cd $HOME
+      apt update && apt upgrade -y
+      apt intall php -y
+      apt install git
+      git clone https://github.com/Tuhinshubhra/RED_HAWK
+      echo '\33[31;1minstalling succes....! \33[37;1m'
+      sleep 1
+	exit
+      
+  elif [ $pil = '2' ];
+  then
+      echo '\33[31;1minstalling darkfb\33[37;1m'
+      cd $HOME
+      pkg update
+      pkg upgrade
+      pkg install git
+      pkg install python2
+      git clone https://github.com/kumpulanremaja/darkfb
+      echo '\33[31;1minstalling succes....!\33[37;1m'
+      sleep 1
+	exit
+  elif [ $pil = '3' ];
+  then
+      echo '\33[31;1minstalling melacak informasi\33[37;1m'
+      cd $HOME
+      apt update
+      apt upgrade
+      pkg install python git
+      git clone https://github.com/maldevel/IPGeoLocation
+      echo '\33[31;1minstalling succes....!\33[37;1m'
+      sleep 1
+      echo '\33[31;1msetelah masuk ke folder nya ketik chmod +x ipgeolocation.py'
+      echo 'lalu pip install -r repuirements.txt\33[37;1m'
+     exit
+  elif [ $pil = '4' ];
+  then
+      echo '33[31;1minstalling lazymux\33[37;1m'
+      cd $HOME
+      pkg update && pkg upgrade
+      pkg install python2 git
+      git clone https://github.com/Gameye98/Lazymux
+      echo '\33[31;1minstalling succes....!\33[37;1m'
+	sleep 1
+	exit
+  elif [ $pil = '5' ];
+  then
+      echo '\33[31;1minstalling osif\33[37;1m'
+      cd $HOME
+      pkg install python2
+      git clone https://github.com/ciku370/OSIF
+      echo '\33[31;1minstalling succes'
+      echo '\33[31;1mjangan lupa request nya, ketik seperti ini/copas pip2 install -r requirements.txt\33[37;1m'
+	exit 
+
+  elif [ $pil = '6' ];
+  then
+      echo '\33[31;1minstalling spam call masal\33[37;1m'
+      cd $HOME
+      apt update
+	apt upgrade
+	apt install curl
+	git clone https://github.com/0x7G3N99/BEEPBEEPSPAMMERA
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	sleep 1
+	exit
+	
+  elif [ $pil = '7' ];
+  then
+	echo '\33[31;1minstalling spam sms...\33[37;1m'
+	cd $HOME
+	pkg install update && pkg install upgrade
+	pkg install python
+	pkg install git
+	git clone https://github.com/KANG-NEWBIE/SpamSms
+	echo '\33[31;1minstalling succes...!\33[37;1m'
+	sleep 1
+	exit
+
+  elif [ $pil = '8' ];
+  then
+	echo '\33[31;1minstalling Auto MBF...!\33[37;1m'
+	cd $HOME
+	pkg install update && pkg install upgrade
+	pkg install git
+	pkg install python
+	pip install bs4
+	git clone https://github.com/FR13ND8/BruteFb
+	echo '\33[31;1minstalling succes...!\33[37;1m'
+	sleep 1
+	exit
+
+  elif [ $pil = '9' ];
+  then
+	echo '33[31;1minstalling katoolin....!\33[37;1m'
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install git
+	pkg install python2
+	pkg install nano
+	pkg install gnupg
+	git clone https://github.com/LionSec/katoolin.git
+	echo 'installing succes...\33[37;1m'
+	echo '\33[31;1mSetelah ituÂ  ketik\33[37;1m'
+	
+echo ' ls
+ cd katoolin
+ ls
+ python2 katoolin.py
+ nano katoolin.py
+Lalu kalian cari nomer 7 trus masukan nomer listriknya/ nomer PLN lalu enterr...
+Hbis itu tekan tandan perintah ini "ls" dan jangan lupa hilangin tanda kutipnya oke\33[37;1m'
+	slepp 1
+	exit
+exit
+  elif [ $pil = '10' ];
+  then
+	echo '\33[31;1minstalling spam v1...!\33[37;1m'
+	cd $HOME
+	gem install lolcat
+	pip2 install toilet
+	pip2 install lolcat
+	pkg install php
+	git clone https://github.com/GUNAWAN18ID/B3604-spam-v1.git
+	sleep 1
+	exit
+  elif [ $pil = '11' ];
+  then
+	echo '\33[31;1minstalling Nuyul Caping\33[37;1m'
+	cd $HOME
+	apt update && apt upgrade
+	apt install git
+	apt install php
+	git clone https://github.com/anggaid14/caping
+	sleep 1
+	echo 'installing succes...!\33[37;1m'
+	exit
+  elif [ $pil = '12' ];
+  then
+	echo '\33[31;1minstalling virus hp\33[37;1m'
+	cd $HOME
+	apt update && apt upgrade
+	apt install git python2 -y
+	git clone https://github.com/jota01234/Vbug
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	exit
+  elif [ $pil = '13' ];
+  then
+	echo '\33[31;1minstalling cashtree\33[37;1m'
+	cd $HOME
+	pkg install update && pkg upgrade
+	pkg install php
+	pkg install git
+	pkg install nano
+	git clone https://github.com/radenvodka/cashtree
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	exit
+  elif [ $pil = '14' ];
+  then
+	echo '\33[31;1minstalling auto like instagram \33[37;1m'
+	cd $HOME
+	pkg install git
+	git clone https://github.com/kumpulanremaja/ig
+	cd ig
+	unzip node_modules.zip
+	pkg install nodejs-lts
+	npm install instagram-private-api
+	node index.js
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	exit
+  elif [ $pil = '15' ];
+  then
+	echo '\33[31;1minstalling H target no brute\33[37;1m'
+	cd $HOME
+	pkg install update && pkg upgrade
+	pkg install python2
+	pkg install git
+	pip install requests
+	pip install mechanize
+	git clone https://github.com/Oseid/Facebom.git
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m
+	exit'
+  elif [ $pil = '16' ];
+  then
+	echo '\33[31;1minstalling sadap WA V2\33[37;1m'
+	cd $HOME
+	pkg update && pkg upgrade
+	pkg install git
+	pkg install curl
+	git clone https://github.com/AndriGanz/whatshack
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	exit
+  elif [ $pil = '17' ];
+  then
+	echo '\33[31;1minstalling metasploit\33[37;1m'
+	cd $HOME
+	pkg install unstable-repo
+	pkg install metasploit
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+ 	exit 
+  elif [ $pil = '18' ];
+  then
+	echo '\33[31;1minstalling DDOS HAMMER\33[37;1m'
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install python
+	pkg install git
+	git clone https://github.com/cyweb/hammer
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	exit
+  elif [ $pil = '19' ];
+  then
+	echo '\33[31;1minstalling virtex \33[37;1m'
+	cd $HOME
+	apt update && apt upgrade
+	apt install git
+	apt install ruby
+	apt install figlet
+	gem install lolcat
+	apt install curl
+	git clone https://github.com/muhammadfathul/VIRTEX
+	sleep 1
+	echo '\33[31;1msetelah masuk ke folder virtext nya kasih permission nya dengan cara , chmod 777 virtex.sh\33[37;1m'
+	sleep 1
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	exit
+  elif [ $pil = '20' ];
+  then
+	echo '\33[31;1minstalling phising\33[37;1m'
+	cd $HOME
+	pkg update && pkg upgrade
+	pkg install wget
+	pkg install openssh
+	pkg install php
+	pkg install curl
+	git clone https://github.com/thelinuxchoice/shellphish/
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	sleep 1
+	exit
+  elif [ $pil = '21' ];
+  then
+	echo '\33[31;1minstalling tombol special\33[37;1m'
+	cd $HOME
+	pkg update && pkg upgrade
+	pkg install python -y
+	pkg install git -y
+	git clone https://github.com/kumpulanremaja/termuxtbbcd termuxtbb
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	sleep 1
+	exit
+  elif [ $pil = '22' ];
+  then
+	echo '\33[31;1minstalling un friend\33[37;1m'
+	cd $HOME
+	pkg install php
+	pkg install git
+	pkg update
+	pkg upgrade
+	git clone https://github.com/dandyraka/FacebookUnfriend
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	sleep 1
+	exit
+  elif [ $pil = '23' ];
+  then
+	echo '\33[0;36minstalling jadwal sholat\33[37;1m'
+	cd $HOME
+	pkg update && pkg upgrade
+	pkg install python && pkg install git
+	git clone https://github.com/karjok/sholat/
+	echo '\33[31;1minstalling succes....!\33[37;1m'
+	sleep 1
+	exit
+  elif [ $pil = '0' ];
+  then
+      echo '\33[31;1mterima kasih telah berkunjung :)\033[1;0m'
+      sleep 1
+      exit
+  else
+      echo '\033[0;31m┌∩┐(◣_◢)┌∩┐
+WRONG INPUT !!!! \033[1;0m'
+      sleep 1
+      echo $ulang
+  fi
+done
